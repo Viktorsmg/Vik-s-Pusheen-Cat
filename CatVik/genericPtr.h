444f0e1249210c98282c112c821a048239cc7e94
Viktorsmg <Viktor.smg@gmail.com>
2019-04-08 20:54:02 +0300

    Initial unfinished implementation of generic pointer
    Points to generic type, allows for the usage of generic functions like addition or stream IO.
    Will be used to generalise the cat menu.

---
+++ CatVik/genericPtr.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iostream>
+using std::ostream;
+using std::istream;
+using std::string;
+
+class genericPtr {
+
+	//0 - nullptr
+	//1 - int
+	//2 - double
+	//3 - bool
+	void *ptr;
+
+	//0 - nullptr
+	//1 - int
+	//2 - double
+	//3 - bool
+	signed char type;
+
+	//true - points to an object it owns (created), will delete when it dies
+	//false - points to some other object, can be used as universal access, no delete
+	bool selfcontained;
+
+	void set(const genericPtr &val);
+	void unset();
+
+public:
+
+
+	genericPtr();
+	genericPtr(const int &val);
+	genericPtr(int *val);
+	genericPtr(const double &val);
+	genericPtr(double *val);
+	genericPtr(const bool &val);
+	genericPtr(bool *val);
+
+	genericPtr(const genericPtr &val);
+
+	~genericPtr();
+
+	genericPtr& operator=(const genericPtr &val);
+
+	double get_double() const;
+	signed char get_type() const;
+	bool is_selfcontained() const;
+
+	string to_string() const;
+
+	genericPtr& operator+=(int val);
+
+	friend ostream& operator<<(ostream& os, const genericPtr &ptr);
+	friend istream& operator>>(istream& os, genericPtr &ptr);
+};
+
